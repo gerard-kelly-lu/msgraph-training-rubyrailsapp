8d85e2f6a726bcddcca7a975479dfd088efa09a1
Gerard Kelly <gerard.kelly@learnupon.com>
2020-09-24 12:00:54 +0100

    Adding missing file.

---
+++ demo/graph-tutorial/config/oauth_environment_variables.rb
@@ -0,0 +1,3 @@
+ENV['AZURE_APP_ID'] = 'CHANGE-ME'
+ENV['AZURE_APP_SECRET'] = 'CHANGE-ME'
+ENV['AZURE_SCOPES'] = 'openid profile user.read calendars.read OnlineMeetings.ReadWrite Calendars.ReadWrite'
